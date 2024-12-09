b0e1e1acfa62a0097bfcba48bcb0a7de22d6ad92
han-suh <grnadbleu@naver.com>
2024-12-09 18:12:21 +0900

    Add files via upload

---
+++ ajax/l04document_load.js
@@ -0,0 +1,9 @@
+//window.onload=test;
+//img나 영상의 로딩을 늦추는 기법(lazy)이 유행이고 dom이 로딩되는 시점은 여러번 재정의하기 때문에
+//window.onload는 거의 사용하지 않는다.
+//document.addEventListener("DOMContentLoaded",test);//*****
+
+function test(){
+    console.log(title);
+}
+test();
